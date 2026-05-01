b4bf19ae3691c43766ff0e3bebc9035114fb025b
Zachary Harder <zharder7@gamil.com>
2026-05-01 14:03:25 -0700

    fixed lag problem

--- packages/frontend/src/Grid.jsx
+++ packages/frontend/src/Grid.jsx
@@ -1,35 +1,61 @@
 import { useState } from "react";
+
 function Grid() {
-    const [rows, setRows] = useState(4);
-    const [cols, setCols] = useState(4);
-    const total = rows * cols;
-    const items = Array.from({ length: total });
+  const [rows, setRows] = useState(4);
+  const [cols, setCols] = useState(4);
+
+  const total = rows * cols;
+  const items = Array.from({ length: total });
+
   return (
-  <body>
     <div className="grid">
-        <h2 style={{paddingLeft: '150px',color: 'white'}}>x by x box</h2>
-        <div style={{ display: 'flex',paddingLeft: '150px' }}>
-         <input
-            style={{ width: '50px', padding: '4px', fontSize: '14px' }}
-            type="index"
-            min="1"
-            max="10"
-            value={rows}
-            onChange={(e) => setRows(Number(e.target.value))}
-            />
-            <h2 style={{color: 'white'}}>X</h2>
-            <input
-                style={{ width: '50px', padding: '4px', fontSize: '14px' }}
-                type="index"
-                min="1"
-                max="10"
-                value={cols}
-                onChange={(e) => setCols(Number(e.target.value))}
-            />
-            </div>
+      <h2 style={{ paddingLeft: "150px"}}>x by x box</h2>
+
+      <div style={{ display: "flex", paddingLeft: "150px" }}>
+        <input
+          style={{ width: "50px", padding: "4px", fontSize: "14px" }}
+          min="1"
+          max="10"
+          type="number"
+          value={rows}
+          onChange={(e) => {
+            let val = e.target.value;
+            if (val === "") return setRows(""); // allow typing
+
+            val = Number(val);
+            if (val > 10) val = 10;
+            if (val < 1) val = 1;
+
+            setRows(val);
+          }}
+        />
+
+        <h2>X</h2>
+
+        <input
+          min="1"
+          max="10"
+          style={{ width: "50px", padding: "4px", fontSize: "14px" }}
+          type="number"
+          value={cols}
+          onChange={(e) => {
+            let val = e.target.value;
+            if (val === "") return setCols("");
+
+            val = Number(val);
+            if (val > 10) val = 10;
+            if (val < 1) val = 1;
+
+            setCols(val);
+          }}
+        />
+      </div>
+
       <div className="container">
-        <div className="grid-box"
+        <div
+          className="grid-box"
           style={{
+            display: "grid",
             gridTemplateColumns: `repeat(${cols}, 100px)`,
             gridTemplateRows: `repeat(${rows}, 100px)`,
           }}
@@ -42,7 +68,6 @@ function Grid() {
         </div>
       </div>
     </div>
-  </body>
   );
 }
 
