74cc3ec46f3fd48170d8e5bcf66dd1138e778363
Fabian Chitay <fchitay@calpoly.edu>
2026-05-17 09:45:24 -0700

    changed colors, added rounded edges, etc

--- packages/frontend/src/Grid.jsx
+++ packages/frontend/src/Grid.jsx
@@ -13,9 +13,9 @@ function Grid() {
   const cellSize = Math.min(100, maxSize / Math.max(safeRows, safeCols));
 
   useEffect(() => {
-  document.documentElement.style.setProperty(
-    "--matrix-cell-size",
-    `${cellSize}px`
+    document.documentElement.style.setProperty(
+      "--matrix-cell-size",
+      `${cellSize}px`,
     );
   }, [cellSize]);
 
