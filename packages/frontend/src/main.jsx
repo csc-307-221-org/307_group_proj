13e975ac75c75668e6189daeaab28c0eb367473c
Zachary Harder <89939156+zackyboy123456@users.noreply.github.com>
2026-05-26 23:49:22 -0700

    Refactor imports in main.jsx for better clarity

--- packages/frontend/src/main.jsx
+++ packages/frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import ReactDOMClient from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
